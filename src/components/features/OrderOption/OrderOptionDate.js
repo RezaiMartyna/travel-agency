4d149175b808b5cba98df52dda7709d2bf4493a2
Martyna Rezai <martyna.rezai@gmail.com>
2021-02-23 21:57:49 +0100

    19.3

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,16 +1,28 @@
 
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 import DatePicker from 'react-datepicker';
 
-const OrderOptionDate = () => {
-  const [startDate, setStartDate] = useState(new Date());
-  return (
-    <div className={styles.component}>
-      <DatePicker selected={startDate} onChange={date => setStartDate(date)} />
-    </div>
-  );
-};
+const OrderOptionDate = ({ currentValue, setOptionValue, name }) => (
+  <div className={styles.component}>
+    <DatePicker
+      type="date"
+      className={styles.input}
+      selected={currentValue}
+      onChange={(date) => setOptionValue(date)}
+      name={name}
+      placeholderText={'pick a day'}
+    />
+  </div>
+);
 
+OrderOptionDate.propTypes = {
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  name: PropTypes.string,
+  startDate: PropTypes.string,
+  setStartDate: PropTypes.string,
+};
 export default OrderOptionDate;
